5e7f4331062c3ec5be74a32179cfe8eba69eb53e
Oskar Berg <oskar.o.a.berg@gmail.com>
2024-06-11 17:19:07 +0200

    wip

--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
+	import '../../node_modules/@odevo/ui/src/styles/root-styles.css';
+	import '../styles/base-styles.css';
 
 	onMount(async () => {
 		await import('@odevo/ui');
 	});
 </script>
 
-<odv-test />
+<odv-button />
